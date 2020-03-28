aa69fd20ae3a43009eeb77eb2294d85b03185ad0
Mehmet Ozdemir <mozdemir2408@gmail.com>
2020-03-28 22:20:29 +0300

    One match fight is converted to a tournament system, Slow and Fast modes are added.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,20 +7,68 @@ import java.util.List;
 public class Main {
 
     private static Faker faker = new Faker();
+    private static boolean isSlowMode = false;
 
     public static void main(String[] args) {
+        isSlowMode = isSlowMode(args);
+        int characterSize = 16;
 
-        List<Character> characterList = getCharacters(10);
-        Pair pair = getPair(characterList);
-        try {
-            Game game = fight(pair);
-            System.out.println(game);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        List<Character> characterList = getCharacters(characterSize);
+
+        List<Character> winnerList = new ArrayList<>();
+        List<Character> loserList = new ArrayList<>();
+
+        int totalRound = getTotalRound(characterSize);
+
+        for (int i = 0; i < totalRound; i++) {
+            do {
+                Pair pair = getPair(characterList);
+                try {
+                    Game game = fight(pair);
+                    System.out.println(game);
+
+                    characterList.remove(game.getWinner());
+                    characterList.remove(game.getLoser());
+
+                    winnerList.add(game.getWinner());
+                    loserList.add(game.getLoser());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } while (characterList.size() != 0);
+
+            printRoundResult(winnerList, loserList);
+            characterList.addAll(winnerList);
+            winnerList.clear();
+            loserList.clear();
+        }
+
+        System.out.println();
+
+        System.out.println(characterList.get(0).getName() + " is the Champion!");
+    }
+
+    private static boolean isSlowMode(String[] args) {
+        String key = args[0];
+        String value = args[1];
+
+        if (key.equals("SLOW_MODE")) {
+            return value.equals("true");
+        } else {
+            return false;
         }
     }
 
-    public static List<Character> getCharacters(int size) {
+    private static int getTotalRound(int characterSize) {
+        int totalRound = 0;
+        do {
+            characterSize = characterSize / 2;
+            totalRound++;
+        } while (characterSize != 1);
+        return totalRound;
+    }
+
+    private static List<Character> getCharacters(int size) {
         List<Character> characters = new ArrayList<>();
 
         for (int i = 0; i < size; i++) {
@@ -31,7 +79,7 @@ public class Main {
         return characters;
     }
 
-    public static Pair getPair(List<Character> characters) {
+    private static Pair getPair(List<Character> characters) {
         int randomIndex1;
         int randomIndex2;
 
@@ -46,7 +94,7 @@ public class Main {
         return new Pair(character1, character2);
     }
 
-    public static Game fight(Pair pair) throws InterruptedException {
+    private static Game fight(Pair pair) throws InterruptedException {
         Character character1 = pair.getCharacter1();
         Character character2 = pair.getCharacter2();
         int round = 0;
@@ -66,11 +114,25 @@ public class Main {
 
             System.out.println("____________________________________________");
 
-            Thread.sleep(1000);
+            if (isSlowMode) Thread.sleep(1000);
+
             round++;
         } while (character1.getHealth() > 0 && character2.getHealth() > 0);
 
         if (character1.getHealth() <= 0) return new Game(character2, character1, round);
         else return new Game(character1, character2, round);
+    }
+
+    private static void printRoundResult(List<Character> winnerList, List<Character> loserList) {
+        System.out.println("Winners : ");
+        for (Character winner : winnerList) {
+            System.out.println(winner.getName());
+        }
+        System.out.println();
+        System.out.println("Losers: ");
+        for (Character loser : loserList) {
+            System.out.println(loser.getName());
+        }
+        System.out.println();
     }
 }
